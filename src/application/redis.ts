96a80d3b8975a94b1dec0c1f7591eb9e0ab499b6
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-10-02 15:27:30 +0700

    implemen cache using redis

---
+++ src/application/redis.ts
@@ -0,0 +1,11 @@
+import Redis from "ioredis"
+import { logger } from "./logger"
+
+export const redis = new Redis({
+    host: "127.0.0.1",
+    port: 6379
+})
+
+redis.on("connect",()=>{
+    logger.info("Redis connext")
+})
--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,8 +1,9 @@
 import {Router} from "express"
 import { ProductController } from "../controllers/product-controller"
+import { cacheMiddleware } from "../middleware/cache-middleware"
 const productRouter = Router()
 
 productRouter.post("/",ProductController.create)
-productRouter.get("/",ProductController.get)
+productRouter.get("/",cacheMiddleware("product",500),ProductController.get)
 
 export default productRouter
